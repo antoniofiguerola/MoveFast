16817020098c559805be7a65b2d260b464c9f107
maria <maria@gmail.com>
2018-05-11 17:57:26 +0200

    Prova

--- src/movefast/MoveFast.java
+++ src/movefast/MoveFast.java
@@ -15,7 +15,7 @@ public class MoveFast {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("esper que vagi be");
     }
     
 }
