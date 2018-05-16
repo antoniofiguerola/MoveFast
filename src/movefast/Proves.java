31eaddcd2cba19d75b2c70f2b92bd7ccc75b06cb
Informatica <antoniofiguerola@iespaucasesnoves.cat>
2018-05-16 18:48:25 +0200

    Insertar clase enum TipusPagament i modificar clases asociades

--- src/movefast/Proves.java
+++ src/movefast/Proves.java
@@ -27,9 +27,9 @@ public class Proves {
         Cotxe cotxe1 = new Cotxe("1234abc", "123456789", "Peugeot", "308", 5, 80.00, true, 5);
         Cotxe cotxe2 = new Cotxe("4567def", "987654321", "Citroen", "C4 Picasso", 5, 70.00, true, 5);
         Camio camio1 = new Camio("1357AGH", "13579", "IVECO", "Superior", 3, 150.00, true, 12, 12000);
-        Casual cli1 = new Casual("Antonio", "Figuerola", "43135006R", "Ca meva", 971971971, "Efectiu");
-        Casual cli2 = new Casual("Marc", "Grossi", "12345678A", "Can Grossi", 871871871, "Efectiu");
-        Casual cli3 = new Casual("Miquel", "Arrom", "87654321B", "Can Miquel", 671671671, "Efectiu");
+        Casual cli1 = new Casual("Antonio", "Figuerola", "43135006R", "Ca meva", 971971971, TipusPagament.COMPTAT);
+        Casual cli2 = new Casual("Marc", "Grossi", "12345678A", "Can Grossi", 871871871, TipusPagament.TARJETACREDIT);
+        Casual cli3 = new Casual("Miquel", "Arrom", "87654321B", "Can Miquel", 671671671, TipusPagament.TARJETADEBIT);
         EmpresaLeasing empLea1 = new EmpresaLeasing("Empresa", "Ca s'Empresa", 971971971, "empresa@empresa.cat");
         Leasing clil1 = new Leasing("Andreu", "Apellido", "15935746Z", "Direccio", 951357468, empLea1);
         Lloguer lloguer1 = new Lloguer(50.0, LocalDate.of(2018, 5, 16), LocalDate.of(2018, 5, 20), cotxe2, cli3);
--- src/movefast/client/Casual.java
+++ src/movefast/client/Casual.java
@@ -7,25 +7,25 @@ package movefast.client;
 public class Casual extends Client {
 
     //Atributs
-    protected String tipusPagament; //fer enum
+    protected TipusPagament tipusPagament; //fer enum
 
     //constructor
-    public Casual(String nom, String cognom, String dni, String direccio, int telefon, String tipusPagament) {
+    public Casual(String nom, String cognom, String dni, String direccio, int telefon, TipusPagament tipusPagament) {
         super(nom, cognom, dni, direccio, telefon);
         this.tipusPagament = tipusPagament;
     }
 
-    public String getTipusPagament() {
+    public TipusPagament getTipusPagament() {
         return tipusPagament;
     }
 
-    public void setTipusPagament(String tipusPagament) {
+    public void setTipusPagament(TipusPagament tipusPagament) {
         this.tipusPagament = tipusPagament;
     }
 
     @Override
     public String toString() {
-        return super.toString() + "Casual{" + "tipusPagament=" + tipusPagament + '}';
+        return "Casual{" + "tipusPagament=" + tipusPagament + '}';
     }
 
 }
